93b0144187ea113cba326207217a7dbe4af94a46
rvalente <rogerio.valente@capgemini.com>
2022-05-11 19:08:54 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,13 @@
 import Pagination from "components/Pagination";
+import MovieCard from "components/MovieCard";
 
 function Listing() {
-  return <Pagination />;
+  return (
+    <>
+      <Pagination />
+      <MovieCard />
+    </>
+  );
 }
 
 export default Listing;
