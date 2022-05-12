e6370560f069bf254936c3cb3c53e1edb06e6c2b
rvalente <rogerio.valente@capgemini.com>
2022-05-12 13:43:37 -0300

    Navigations buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,32 +1,33 @@
-import "./styles.css";
+import { Link } from 'react-router-dom';
+import './styles.css';
 
 function Form() {
   const movie = {
     id: 1,
     image:
-      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
+      'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg',
+    title: 'The Witcher',
     count: 2,
     score: 4.5,
   };
 
   return (
-    <div className="dsmovie-form-container">
+    <div className='dsmovie-form-container'>
       <img
-        className="dsmovie-movie-card-image"
+        className='dsmovie-movie-card-image'
         src={movie.image}
         alt={movie.title}
       />
-      <div className="dsmovie-card-bottom-container">
+      <div className='dsmovie-card-bottom-container'>
         <h3>{movie.title}</h3>
-        <form className="dsmovie-form">
-          <div className="form-group dsmovie-form-group">
-            <label htmlFor="email">Informe seu email</label>
-            <input type="email" className="form-control" id="email" />
+        <form className='dsmovie-form'>
+          <div className='form-group dsmovie-form-group'>
+            <label htmlFor='email'>Informe seu email</label>
+            <input type='email' className='form-control' id='email' />
           </div>
-          <div className="form-group dsmovie-form-group">
-            <label htmlFor="score">Informe sua avaliação</label>
-            <select className="form-control" id="score">
+          <div className='form-group dsmovie-form-group'>
+            <label htmlFor='score'>Informe sua avaliação</label>
+            <select className='form-control' id='score'>
               <option>1</option>
               <option>2</option>
               <option>3</option>
@@ -34,13 +35,15 @@ function Form() {
               <option>5</option>
             </select>
           </div>
-          <div className="dsmovie-form-btn-container">
-            <button type="submit" className="btn btn-primary dsmovie-btn">
+          <div className='dsmovie-form-btn-container'>
+            <button type='submit' className='btn btn-primary dsmovie-btn'>
               Salvar
             </button>
           </div>
         </form>
-        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        <Link to='/'>
+          <button className='btn btn-primary dsmovie-btn mt-3'>Cancelar</button>
+        </Link>
       </div>
     </div>
   );
